5a007797bc970d443b6128a0cfbf0502e8d7db44
Vinffs <vin.demiranda@gmail.com>
2024-03-28 15:09:03 +0100

    Snack 3 - Given an Array of Integers, sum the numbers on odd position.

---
+++ src/org/learning/snackthree/SnackThree.java
@@ -0,0 +1,18 @@
+package org.learning.snackthree;
+
+public class SnackThree {
+    public static void main(String[] args) {
+
+        int[] numbers = {53, 17, 89, 42, 65, 28, 91, 34, 76, 10};
+
+        int counter = 2;
+        int sum = 0;
+
+        for (int i = 1; i <= numbers.length ; i += counter ) {
+            sum += numbers[i];
+        }
+
+        System.out.println("The sum of odd position numbers is " + sum);
+
+    }
+}
