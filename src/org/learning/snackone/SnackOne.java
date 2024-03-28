24f5b0758eb7c43357d4314445df47b452124280
Vinffs <vin.demiranda@gmail.com>
2024-03-28 14:31:50 +0100

    Snack 1 - ask user an even number between 10 and 20 , if invalid, keep asking

---
+++ src/org/learning/snackone/SnackOne.java
@@ -0,0 +1,22 @@
+package org.learning.snackone;
+
+import java.util.Scanner;
+
+public class SnackOne {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Insert a number between 10 and 20: ");
+        int num = Integer.parseInt(scan.nextLine());
+
+
+        while((num < 10 || num > 20) || num % 2 != 0) {
+            System.out.print("Number Invalid. Please insert a number between 10 and 20: ");
+            num = Integer.parseInt(scan.nextLine());
+        }
+
+        System.out.println("Your number is valid: " + num);
+
+        scan.close();
+    }
+}
